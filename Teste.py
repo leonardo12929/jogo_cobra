194566035196fee8ddae0599bac319bc0d3079bc
Leozinho <leotudo123456@gmail.com>
2025-11-27 13:44:32 -0300

    commit final

--- Teste.py
+++ Teste.py
@@ -30,9 +30,10 @@ lista_cobra = []
 comprimento_inicial = 5
 morreu = False
 
+
 def Morreu():
     fonte2 = pygame.font.SysFont('arial',20,True,True)
-    mensagem = 'Game over! R para reiniciar'
+    mensagem = 'MORREU'
     texto_formatado = fonte2.render(mensagem, True, (0,0,0))
     morreu = True
     while morreu:
@@ -42,26 +43,49 @@ def Morreu():
                     pygame.quit()
                     exit()
                 if event.type == KEYDOWN:
-                    if event.key == K_r:
-                        reiniciar_jogo()
                         return
-            tela.blit(texto_formatado,(200,200))
+            tela.blit(texto_formatado,(270,200))
             pygame.display.update()
 
 def aumenta_cobra(lista_cobra):
     for XeY in lista_cobra:
         pygame.draw.rect(tela,(0,176,240),(XeY[0], XeY[1],20,20))
 
-def reiniciar_jogo():
-    global  comprimento_inicial, x_cobra, y_cobra, lista_cobra, lista_cabeca, x_maca, y_maca , morreu
-    comprimento_inicial = 5
-    x_cobra = int(LARGURA / 2)
-    y_cobra = int(ALTURA / 2)
-    lista_cobra = []
-    lista_cabeca = []
-    x_maca = randint(40,600)
-    y_maca = randint(50,430) 
-    morreu = False   
+
+def pedral():
+    pedra1 = pygame.Rect(200,200,20,20)
+    pygame.draw.circle(tela,(190,190,190), pedra1.center,8)
+
+    pedra2 = pygame.Rect(200,400,20,20)
+    pygame.draw.circle(tela,(190,190,190), pedra2.center,8)
+
+    pedra3 = pygame.Rect(450,300,20,20)
+    pygame.draw.circle(tela,(190,190,190), pedra3.center,8)
+
+    pedra4 = pygame.Rect(500,150,20,20)
+    pygame.draw.circle(tela,(190,190,190), pedra4.center,8)
+    
+    pedra5 = pygame.Rect(50,100,20,20)
+    pygame.draw.circle(tela,(190,190,190), pedra5.center,8)
+    
+
+    
+    if cobra.colliderect(pedra1):
+        colisao.play()
+        Morreu()
+    elif cobra.colliderect(pedra2):
+        colisao.play()
+        Morreu()
+    elif cobra.colliderect(pedra3):
+        colisao.play()
+        Morreu()
+    elif cobra.colliderect(pedra4):
+        colisao.play()
+        Morreu()
+    elif cobra.colliderect(pedra5):
+        colisao.play()
+        Morreu()
+
 
 while True:
     tela.fill((255,255,255))
@@ -100,12 +124,15 @@ while True:
     cobra = pygame.draw.rect(tela,(0, 0, 255), (x_cobra,y_cobra, 20,20))
     maca = pygame.Rect(x_maca, y_maca, 20, 20)
     pygame.draw.circle(tela, (255, 0, 0), maca.center, 10)
-    
+
+    pedral()
+
     if cobra.colliderect(maca):
         x_maca = randint(40,600)
         y_maca = randint(50,430)
+        velocidade += 0.050
         colisao2.play()
-        comprimento_inicial += 1000
+        comprimento_inicial += 10
     lista_cabeca = []   
     lista_cabeca.append(x_cobra)
     lista_cabeca.append(y_cobra)
@@ -113,7 +140,6 @@ while True:
     
     if lista_cobra.count(lista_cabeca ) > 1:
         colisao.play()
-        Morreu()
 
 
     if x_cobra > LARGURA:
